0d8b489460ffb9b2f5c6a17f7b5d84b7fdfd1cf4
jzubak <jzubak81@gmail.com>
2019-02-27 22:12:48 -0500

    WED2

--- hangman.js
+++ hangman.js
@@ -5,20 +5,44 @@
         // guessword in array form
         var wordarray = []
         // User guess 
-        var letterGuess
+        var letterguess
         // Incorrect guesses
         var guessList = []
         // Remaining guesses
         var guessesLeft = 12;
         // Word array made from "_" and spaces
         var dashArray = [];
+        //wins & losses tallys
+        var wins = " ";
+        var loss = " ";
 
+        // function play() {
+        //     // function generateword(){
+                
+        //     for (var i = 0; i < words.length; i++) {
+        //         guessWord = words[Math.floor(Math.random() * words.length)];
+        //     }
+        //     console.log(guessWord)
+        //     // convert guessword to array
+        //     wordarray = guessWord.split("")
+        //     console.log(wordarray)
 
+        //     // convert each letter into an "_" leaving in the spaces. for loop with if else statement
+        //     dashArray = wordarray.map(function (element) {
+        //         return element === " " ? "&nbsp;&nbsp;&nbsp;" : "_";
+        //     })
+
+        //     //push dasharray to the html
+        //     document.getElementById("guessword").innerHTML = dashArray;
+
+        //     // };
+        // }
 
         //Randomly select from the array (computer)
         document.onkeyup = function (event) {
             function play() {
                 // function generateword(){
+                    
                 for (var i = 0; i < words.length; i++) {
                     guessWord = words[Math.floor(Math.random() * words.length)];
                 }
@@ -36,7 +60,7 @@
                 document.getElementById("guessword").innerHTML = dashArray;
 
                 // };
-
+            }
                 if (event.keyCode > 64 && event.keyCode < 91) {
                     letterguess = event.key;
 
@@ -45,7 +69,7 @@
                     let letterFound = false
                     dashArray = dashArray.map(
                         function (element, idx) {
-                            const letterMatch = wordarray[idx].toLowerCase() === letterguess;
+                            const letterMatch = wordarray[idx] === letterguess;
                             if (letterMatch) {
                                 letterFound = true
                                 return wordarray[idx]
@@ -57,30 +81,29 @@
                     )
                     if (letterFound === false) {
                         guessesLeft--
+                        document.getElementById("guessedletters").innerHTML = guessList++;
                         guessList[event.key] = true
                         if (guessesLeft === 0) {
-
-                            var guessText = document.getElementById("guessesLeft");
-                            guessText.textContent = "Guesses Left: " + guessCount;
-
-                            alert("You Lose!")
-                            play();
-                        }
+                            alert("You Lose!");
+                            document.getElementById("loss").innerHTML = loss++;
+                            // play();
+                            }
+                            
                     }
 
 
 
                     document.getElementById("guessword").innerHTML = dashArray;
-                    document.getElementById("guessedletters").innerHTML = letterGuess;
+                    document.getElementById("guessedletters").innerHTML = letterguess;
                     document.getElementById("guessesleft").innerHTML = guessesLeft;
                     document.getElementById("wins").innerHTML = wins;
                     document.getElementById("loss").innerHTML = loss;
 
 
-                    console.log(wordarray)
+                   
 
 
                 };
-            }
+            
             play()
         };
